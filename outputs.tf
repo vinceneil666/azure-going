94cf58a6132645354da429dd1bafab6e9aa6f397
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-06 22:19:02 +0100

    misc updates

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "sudnet-1-id" {
   value = module.vnet-01.sub1-id-1
 }
@@ -13,3 +14,4 @@ output "sudnet-2-name" {
 output "Resource-Group-Name" {
   value = module.resgrp-01.resgrp-name
 }
+*/
