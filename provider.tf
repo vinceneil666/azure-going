c93266f9612df0ee05c2724487d420a705997746
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-06 15:20:00 +0100

    heisanndefrf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
     cloud {
     organization = "wastelands"
-    token = "CNl6vEp8KKg5Wg.atlasv1.grp2BK4G8dwiZV1DG4eiPjHVa8VU1lCJ22Vjdbp1PEySy8vunvIdE2kChS1BMr4IPQ0"
+    token = "NW0foLdyceqe3g.atlasv1.UKL92qucyPVcDcUQdUyK98BAQJXzy3kVNFoHyM8CV4y6bazSz2Wzv6cA4nuDaT0nU20"
     workspaces {
       name = "azure-going"
     }
